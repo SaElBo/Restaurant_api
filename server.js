edb135f0c731476b74090e62dc9a078316e254a2
SaElBo <sboukaleb@gmail.com>
2020-08-11 14:26:37 +0200

    basic restaurant routes

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const dotEnv = require('dotenv');
 
+//Route files 
+const restaurants = require ('./routes/restaurant');
+
 // Load env vars
 dotEnv.config({ path: './config/config.env' });
 
 const app = express();
 
+//Mount routers 
+app.use('/api/v1/restaurants', restaurants);
+
 
 const PORT = process.env.PORT || 3000
+
 app.listen(
     PORT,
     console.log(`Server running in ${process.env.NODE_ENV} mode,  on port ${PORT}`)
