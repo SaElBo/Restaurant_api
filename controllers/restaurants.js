ba2c9fe216b95134fe5977ef1e88771b59a495dd
SaElBo <sboukaleb@gmail.com>
2020-08-14 16:06:44 +0200

    basic restaurant pagination

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -16,7 +16,7 @@ exports.getRestaurants = asyncHandler(async (req, res, next) => {
     const reqQuery = { ...req.query };
 
     //Field to esxclude
-    const removeFields = ['select', 'sort'];
+    const removeFields = ['select', 'sort', 'page', 'limit'];
 
     //Loop over removeFields and delete them from reqQuerry
     removeFields.forEach(param => delete reqQuery[param]);
@@ -45,12 +45,39 @@ exports.getRestaurants = asyncHandler(async (req, res, next) => {
         query = query.sort('-createdAt');
     }
 
+    //Pagination
+    const page = parseInt(req.query.page, 10) || 1;
+    const limit = parseInt(req.query.limit,10) || 25;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const total = await Restaurant.countDocuments();
+
+
+    query = query.skip(startIndex).limit(limit);
 
     //Executing querry
     const allRestaurants = await query;
 
+    //Pagination result
+    const pagination = {};
+
+    if(endIndex < total){
+        pagination.next = {
+            page : page + 1,
+            limit
+        }
+     }
+
+     if(startIndex > 0){
+         pagination.prev = {
+             page: page - 1,
+             limit
+         }
+     }
+
+
     //Return response
-    res.status(200).json({ success: true, count: allRestaurants.length, data: allRestaurants });
+    res.status(200).json({ success: true, count: allRestaurants.length,pagination, data: allRestaurants });
 
 });
 
